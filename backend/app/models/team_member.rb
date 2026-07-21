8fb424f6529682ef2da8c81388658221230afb32
Abigail Galzin <abigail.galzin@assuresoft.com>
2026-07-21 13:37:06 -0400

    feat: create database migrations for TeamMember model and added validations

---
+++ backend/app/models/team_member.rb
@@ -0,0 +1,20 @@
+class TeamMember < ApplicationRecord
+  enum role: {
+    developer: 0,
+    qa: 1,
+    support: 2
+  }
+
+  scope :active, -> { where(active: true)}
+
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :email, presence: true,
+    uniqueness: { case_sensitive: false },
+    format: {
+      with: URI::MailTo::EMAIL_REGEXP,
+      message: "must be a valid email address"
+    }
+  validates :role, presence: true, inclusion: { in: role.keys }
+  validates :active, inclusion: { in: [true, false] }
+
+end
