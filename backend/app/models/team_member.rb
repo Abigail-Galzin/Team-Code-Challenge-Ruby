1d1c89626100d9a3b8a9821b45e3fd607bcb4555
Abigail Galzin <abigail.galzin@assuresoft.com>
2026-07-21 15:23:55 -0400

    test: add model and validation unit tests

--- backend/app/models/team_member.rb
+++ backend/app/models/team_member.rb
@@ -1,9 +1,9 @@
 class TeamMember < ApplicationRecord
-  enum role: {
+  enum :role, {
     developer: 0,
     qa: 1,
     support: 2
-  }
+  }, validate: true
 
   scope :active, -> { where(active: true)}
 
@@ -14,7 +14,7 @@ class TeamMember < ApplicationRecord
       with: URI::MailTo::EMAIL_REGEXP,
       message: "must be a valid email address"
     }
-  validates :role, presence: true, inclusion: { in: role.keys }
+  validates :role, presence: true, inclusion: { in: roles.keys }
   validates :active, inclusion: { in: [true, false] }
 
 end
