9daf84c89bdda812f58c4b349d543960a16c7136
Abigail Galzin <abigail.galzin@assuresoft.com>
2026-07-23 17:44:44 -0400

    feat: enhance TeamMembers API with filtering, pagination, and structured responses

--- backend/app/controllers/api/v1/team_members_controller.rb
+++ backend/app/controllers/api/v1/team_members_controller.rb
@@ -1,15 +1,39 @@
 class Api::V1::TeamMembersController < ApplicationController
-  before_action :set_team_member, only: %i[show update destroy]
+  before_action :set_team_member, only: %i[show update]
 
   # GET /api/v1/team_members
   def index
-    team_members = TeamMember.all.order(created_at: :desc)
-    render json: team_members, status: :ok
+    team_members = TeamMember.includes(:support_requests).order(created_at: :desc)
+
+    query_params = request.query_parameters
+    team_members = team_members.by_active(query_params[:active])
+      .by_role(query_params[:role])
+      .search_by_name(query_params[:name])
+
+    pagy, records = pagy(team_members, limit: 10)
+
+    response = Response::ResponseData.new(
+      data: records.as_json(
+        except: [:created_at, :updated_at],
+        include: { support_requests: { only: [:title, :status, :priority]}}
+      ),
+      message: I18n.t('success.response', model: TeamMember.model_name.human.pluralize),
+      status: :ok
+    )
+
+    render json: response.as_json.merge(Response::ResponsePaginationInfo.new(pagy).as_json),
+      status: response.status
   end
 
   # GET /api/v1/team_members/:id
   def show
-    render json: @team_member, status: :ok
+    response = Response::ResponseData.new(
+      data: @team_member.as_json(include: { support_requests: { only: [:title, :status, :priority]}}),
+      message: I18n.t('success.response', model: TeamMember.model_name.human),
+      status: :ok
+    )
+
+    render json: response.as_json, status: response.status
   end
 
   # POST /api/v1/team_members
@@ -17,7 +41,13 @@ class Api::V1::TeamMembersController < ApplicationController
     team_member = TeamMember.new(team_member_params)
 
     if team_member.save
-      render json: team_member, status: :created
+      response = Response::ResponseData.new(
+        data: team_member.as_json(include: { support_requests: { only: [:title, :status, :priority]}}),
+        message: I18n.t('success.response', model: TeamMember.model_name.human),
+        status: :created
+      )
+
+      render json: response.as_json, status: response.status
     else
       render json: { errors: team_member.errors.full_messages }, status: :unprocessable_entity
     end
@@ -26,16 +56,16 @@ class Api::V1::TeamMembersController < ApplicationController
   # PUT/PATCH /api/v1/team_members/:id
   def update
     if @team_member.update(team_member_params)
-      render json: @team_member, status: :ok
+      response = Response::ResponseData.new(
+        data: @team_member.as_json(include: { support_requests: { only: [:title, :status, :priority]}}),
+        message: I18n.t('success.response', model: TeamMember.model_name.human),
+        status: :ok
+      )
+
+      render json: response.as_json, status: response.status
     else
       render json: { errors: @team_member.errors.full_messages }, status: :unprocessable_entity
     end
-  end
-
-  # DELETE /api/v1/team_members/:id
-  def destroy
-    @team_member.destroy
-    head :no_content # Retorna status 204 sin cuerpo
   end
 
   private
